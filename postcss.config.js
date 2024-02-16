6e1154916300e34bce0a9c05637aa2e1844aa55c
Kristján Oddsson <koddsson@gmail.com>
2024-02-16 16:26:15 +0100

    Fix CSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,7 @@
 import postcssJitProps from 'postcss-jit-props';
 import OpenProps from 'open-props';
 import atImport from 'postcss-import';
-import nano from 'cssnano';
 
 export default {
-  plugins: [atImport(), postcssJitProps(OpenProps), nano({preset: 'default'})]
+  plugins: [atImport(), postcssJitProps(OpenProps)]
 }
